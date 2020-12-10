211174aaed6f2c9fc9a99d2ba5664eb3f446727f
JDC1492 <Jonathan_Cohen10@yahoo.com>
2020-12-10 16:07:20 -0500

    db changed for simplistic flow

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,31 +10,16 @@ List.create(name: "Jonathan")
     ListItem.create(item_name: "G-Shock Smart Watch",
         item_price: 399.99,
         list_id:1)
-    ListItem.create(item_name: "Playstation V",
-        item_price: 499.99,
-        list_id:1)
-    ListItem.create(item_name: "Nintendo E-Store Gift Card",
-        item_price: 25.00,
-        list_id:1)
+
 
 List.create(name: "Carlton")
     ListItem.create(item_name: "Nintendo Switch",
         item_price: 299.99,
         list_id:2)
-    ListItem.create(item_name: "Nintendo E-Store Gift Card",
-        item_price: 25.00,
-        list_id:2)
-    ListItem.create(item_name: "Switch Pro Controller",
-        item_price: "69.99",
-        list_id:2)
+
 
 List.create(name: "Ashley")
-    ListItem.create(item_name: "TJ-Maxx Gift Card",
-        item_price: 150.00,
-        list_id:3)
     ListItem.create(item_name: "iPhone 12 Pro Max",
         item_price: 1099.99,
         list_id:3)
-    ListItem.create(item_name: "Old Navy Gift Card",
-        item_price: 150.00,
-        list_id:3)
+   
